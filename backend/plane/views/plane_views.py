dc4e66025e2c3475d740a7184d2fd29ae8760954
Oleksandr Karadiaur <alex.karadyaur@outlook.com>
2024-06-23 15:30:23 +0200

    Added some new views for the plane view

--- backend/plane/views/plane_views.py
+++ backend/plane/views/plane_views.py
@@ -14,3 +14,31 @@ def get_plans(request):
     plane = get_object_or_404(Plane, pk=request.GET.get('pk'))
     serializer = PlaneSerializer(plane)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def create_plane(request):
+    data = request.data
+    plane = PlaneSerializer(data)
+    if plane.is_valid():
+        plane.create(data)
+
+        return Response(plane.data, status=status.HTTP_201_CREATED)
+
+    return Response(plane.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def update_plane(request, pk):
+    plane = get_object_or_404(Plane, pk=pk)
+
+    data = request.data
+    plane.update(data)
+    return Response(plane.data, status=status.HTTP_200_OK)
+
+
+@api_view(['DELETE'])
+def delete_plane(request, pk):
+    plane = get_object_or_404(Plane, pk=pk)
+    plane.delete()
+    return Response(status=status.HTTP_200_OK)
